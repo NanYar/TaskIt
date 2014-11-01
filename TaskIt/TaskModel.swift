0b0beefcd28d2ff0883d0001ad3cb3d2592c6ace
NanYar <nanyar@macbookpro113.fritz.box>
2014-11-01 23:30:30 +0100

    Added code according to tutorial

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,16 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by NanYar on 01.11.14.
+//  Copyright (c) 2014 NanYar. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel
+{
+    var task: String
+    var subTask: String
+    var date: String
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
-    
     @IBOutlet weak var tableView: UITableView!
+    var taskArray: [TaskModel] = []
     
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2014")
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: "01/14/2014")
+        let task3 = TaskModel(task: "Gym", subTask: "Leg day", date: "01/14/2014")
+        
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg day", date: "01/14/2014")] // task3 als alternative Eingabe
+        
+        // Refresh UITableViewDataSource (= die beiden tableView Funktionen ausfuehren)
+        self.tableView.reloadData() // bezieht sich auf: @IBOutlet weak var tableView: UITableView!
     }
 
     override func didReceiveMemoryWarning()
@@ -24,22 +32,34 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+    
+    }
+    
 
     // UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 5
+        return taskArray.count
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell // die Funktion wird taskArray.count mal aufgerufen
     {
-        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        //println(indexPath.row) // indexPath.row zaehlt automatisch von 0 an aufwaerts
+        let thisTask = taskArray[indexPath.row]
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell // bezieht sich auf: func tableView(tableView: UITableView...
+        cell.taskLabel.text = thisTask.task
+        cell.descriptionLabel.text = thisTask.subTask
+        cell.dateLabel.text = thisTask.date
         return cell
     }
     
     // UITableViewDelegate
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
     {
-        
+        println(indexPath.row)
+        performSegueWithIdentifier("showTaskDetail", sender: self)
     }
 }
