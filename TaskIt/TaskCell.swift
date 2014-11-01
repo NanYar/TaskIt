ee5ba5897920d98689837e9301320be07dae897e
NanYar <nanyar@macbookpro113.fritz.box>
2014-11-01 14:53:13 +0100

    Initial code according to tutorial

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,26 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by NanYar on 01.11.14.
+//  Copyright (c) 2014 NanYar. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell
+{
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
